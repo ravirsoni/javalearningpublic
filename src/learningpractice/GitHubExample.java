7bf493f50677819f84355efac725575a9b3feef8
ravirsoni <ravirsoni@gmail.com>
2022-02-21 23:07:39 +0530

    added some code

--- src/learningpractice/GitHubExample.java
+++ src/learningpractice/GitHubExample.java
@@ -5,7 +5,10 @@ public class GitHubExample {
 	public static void main (String[] args) {
 		
 		System.out.println("my first commit");
+		methodWithParams("action");
 		
+		}
+	static void methodWithParams(String a) {
+		System.out.println("method parameter is : "+a);
 	}
-	
 }
